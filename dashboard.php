12e0b75f8c29c55516a4e3531a9b36796e46f66f
Karl Rohr <kjrohr@fullsail.edu>
2016-11-12 15:33:42 -0500

    Fixed everything, working on dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -43,10 +43,10 @@ Hello <?php echo $userRow['user_name']; ?>
  {
   ?>
         <tr>
+        <td><?php echo $row[0]; ?></td>
         <td><?php echo $row[1]; ?></td>
         <td><?php echo $row[2]; ?></td>
         <td><?php echo $row[3]; ?></td>
-        <td><?php echo $row[4]; ?></td>
         </tr>
 
         <?php
