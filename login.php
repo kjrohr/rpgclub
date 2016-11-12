1174a5d4e76cce0cd54794cee85da87679201f3d
Karl Rohr <kjrohr@fullsail.edu>
2016-11-12 15:00:10 -0500

    More debugging

--- login.php
+++ login.php
@@ -1,5 +1,11 @@
 <?php
  session_start();
+ echo "POST DATA:";
+ var_dump($_POST);
+
+
+ echo "SESSION DATA";
+ var_dump($_SESSION);
  require_once 'dbconfig.php';
 
  // it will never let you open index(login) page if session is set
