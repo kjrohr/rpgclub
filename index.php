f1de4ffa9147f096742e59e9fb475407413cd316
Karl Rohr <kjrohr@fullsail.edu>
2017-02-11 04:14:18 -0500

    Reverted to one line

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
   if(isset($_POST['submit-data']))
   {
   $first_name = $_POST['first_name'];
-  $last_name = $_POST['last_name'];
-  $email_address = $_POST['email_address'];
-  $age = $_POST['age'];
-  $gender = $_POST['gender'];
-  $password = $_POST['password'];
+  // $last_name = $_POST['last_name'];
+  // $email_address = $_POST['email_address'];
+  // $age = $_POST['age'];
+  // $gender = $_POST['gender'];
+  // $password = $_POST['password'];
 
-  $sql_query = "INSERT INTO users(first_name,last_name,email_address,age,gender,password) VALUES('$first_name','$last_name','$email_address','$age','$password')";
-
+  //$sql_query = "INSERT INTO users(first_name,last_name,email_address,age,gender,password) VALUES('$first_name','$last_name','$email_address','$age','$password')";
+  $sql_query = "INSERT INTO users(first_name) VALUES('$first_name')";
   mysql_query($sql_query);
 
 }
@@ -25,12 +25,12 @@
 <h1>RPG Club User Registration</h1>
   <form method='post'>
     <input type='text' name="first_name" placeholder='First Name' required />
-    <input type='text' name="last_name" placeholder='Last Name' required />
+    <!-- <input type='text' name="last_name" placeholder='Last Name' required />
     <input type='text' name="email_address" placeholder='Email' required />
     <input type='text' name="age" placeholder='age' required />
     <input type='radio' name='gender' value='m'>Male<br />
     <input type='radio' name='gender' value='f'>Female<br />
-    <input type='password' name='password' placeholder='Password' required />
+    <input type='password' name='password' placeholder='Password' required /> -->
     <button type='submit' name='submit-data'>Submit</button>
   </form>
 </body>
