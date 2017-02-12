00e7390959867aef18d4fdf74d5eb6d7f5a0b1ab
Karl Rohr <kjrohr@fullsail.edu>
2017-02-11 21:54:17 -0500

    Removed hash put back in echo

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
     'cost' => 11,
     'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
 ];
-  //echo $options['salt'];
-  $password = password_hash($pass, PASSWORD_BCRYPT, $options);
+  echo $options['salt'];
+  //$password = password_hash($pass, PASSWORD_BCRYPT, $options);
 
   $sql_query = "INSERT INTO users(first_name,last_name,email_address,age,gender,password,password2) VALUES('$first_name','$last_name','$email_address','$age','$gender','$password','$salt')";
   mysql_query($sql_query);
