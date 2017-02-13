60d51597658fa3a92bbfadade099a79b4a4655d8
Karl Rohr <kjrohr@fullsail.edu>
2017-02-12 20:42:22 -0500

    Made dash basic again

--- dashboard.php
+++ dashboard.php
@@ -8,68 +8,20 @@
   exit;
  }
  // select loggedin users detail
- $res=mysql_query("SELECT * FROM admin WHERE userId=".$_SESSION['user']);
+ $res=mysql_query("SELECT * FROM user WHERE id=".$_SESSION['user']);
  $userRow=mysql_fetch_array($res);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Welcome - <?php echo $userRow['user_name']; ?></title>
+<title>Welcome - <?php echo $userRow['first_name']; ?></title>
 <link rel="stylesheet" href="styles.css" type="text/css" />
 </head>
 <body>
-Hello <?php echo $userRow['user_name']; ?>
+Hello <?php echo $userRow['first_name']; ?>
 <a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a>
 
-<div id="body">
- <div id="content">
-    <table align="center">
-    <tr>
-    <th colspan="8"><a href="index.php">Add Members Here</a></th>
-    </tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Email Address</th>
-    <th>Degree Program</th>
-    </tr>
-    <?php
- $sql_query="SELECT * FROM members";
- $result_set=mysql_query($sql_query);;
- while($row=mysql_fetch_row($result_set))
- {
-  ?>
-        <tr>
-        <td><?php echo $row[0]; ?></td>
-        <td><?php echo $row[1]; ?></td>
-        <td><?php echo $row[2]; ?></td>
-        <td><?php echo $row[3]; ?></td>
-        </tr>
 
-        <?php
- }
- ?>
-    </table>
-
-    <table align="center">
-    <tr>
-    <th>Email Addresses</th>
-    </tr>
-    <?php
-    $sql_query="SELECT * FROM members";
-    $result_set=mysql_query($sql_query);;
-    while($row=mysql_fetch_row($result_set))
-    {
-    ?>
-        <tr>
-        <td><?php echo $row[2]; ?></td>
-        </tr>
-        <?php
-    }
-    ?>
-    </table>
-    </div>
-</div>
-</center>
 </body>
 </html>
