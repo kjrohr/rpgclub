57e40a54daf2d1d26a56d3032e62344e6d256cee
Karl Rohr <kjrohr@fullsail.edu>
2017-03-13 06:39:11 -0400

    Moved some stuff around in nav

--- nav.php
+++ nav.php
@@ -4,14 +4,18 @@
 <link rel="stylesheet" href="styles.css" type="text/css" />
 <nav>
 <span><a href="index.php">Home</a></span>
-<span><a href="login.php">Log In</a></span>
 <span><a href="change_log.php">Change Log</a></span>
 <span><a href="known_issues.php">Known Issues</a></span>
 <?php
     if (isset($_SESSION['user'])) {
       ?>
       <span><a href="suggestions.php">Suggestions</a></span>
-      <span><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></span>
+      <span><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>Sign Out</a></span>
+      <?php
+    }
+    else {
+      ?>
+      <span><a href="login.php">Log In</a></span>
       <?php
     }
 ?>
