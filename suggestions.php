97ee022f8308f297659a16b348726481a12f570a
Karl Rohr <kjrohr@fullsail.edu>
2017-03-13 06:02:06 -0400

    Working on suggestion page

---
+++ suggestions.php
@@ -0,0 +1,30 @@
+<?php
+  session_start();
+  require_once 'dbconfig.php';
+
+  // if session is not set this will redirect to login page
+  if( !isset($_SESSION['user']) ) {
+   header("Location: login.php");
+   exit;
+  }
+
+  if (isset($_POST['submit-post'])) {
+    $user_id = $_SESSION['user'];
+    $content = $_POST['content'];
+    $timestamp = new DateTime();
+    echo $timestamp;
+    $sql_query = "INSERT INTO suggestions(poster_id,timestamp,content) values('$user_id','$timestamp','$content')";
+
+  }
+
+?>
+
+<title>WPRPG Club - Make A Suggestion!</title>
+</head>
+<body>
+  <form method="post">
+    <textarea name='content' rows="20" cols="50">Please make a suggestion.</textarea>
+    <button type="submit" name="submit-post">Make Suggestion</button>
+  </form>
+</body>
+</html>
