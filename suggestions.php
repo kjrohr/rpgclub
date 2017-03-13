e725345fbdadf53bf7f54e8e2f38d317b6bb0f96
Karl Rohr <kjrohr@fullsail.edu>
2017-03-13 06:18:38 -0400

    Starting to push into the database

--- suggestions.php
+++ suggestions.php
@@ -13,9 +13,8 @@
     $content = $_POST['content'];
     $date = getdate();
     $timestamp = $date['weekday'] . ', ' . $date['month'] . ', ' . $date['year'] . ' | ' . $date['hours'] . ':' . $date['minutes'] . ":" . $date['seconds'];
-    echo $timestamp;
     $sql_query = "INSERT INTO suggestions(poster_id,time_stamp,content) values('$user_id','$timestamp','$content')";
-
+    mysql_query($sql_query);
   }
 
 ?>
