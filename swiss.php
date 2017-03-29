df9e4938496ce6c6f78bea86d6d54700d43f8e99
Karl Rohr <kjrohr@fullsail.edu>
2017-03-29 16:19:43 -0400

    Doing a thing

--- swiss.php
+++ swiss.php
@@ -1,7 +1,7 @@
 <?php
   require_once 'dbconfig.php';
 
-  $res=mysql_query("SELECT first_name,last_name FROM t_players");
+  $res=mysql_query("SELECT * FROM t_players");
   $row=mysql_fetch_array($res);
   $count = mysql_num_rows($res);
 ?>
