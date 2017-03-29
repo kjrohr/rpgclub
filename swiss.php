6d353c3be1046090fd9778b521e6ca05d3205a5b
Karl Rohr <kjrohr@fullsail.edu>
2017-03-29 13:12:24 -0400

    Changed  to

--- swiss.php
+++ swiss.php
@@ -1,7 +1,7 @@
 <?php
   require_once 'dbconfig.php';
 
-  $res=mysql_query("SELECT * FROM t_players");
+  $res=mysql_query("SELECT first_name,last_name FROM t_players");
   $row=mysql_fetch_array($res);
   $count = mysql_num_rows($res);
 ?>
@@ -13,7 +13,7 @@
 <body>
   <pre>
   <?php
-    var_dump($row);
+    var_dump($res);
   ?>
   </pre>
 </body>
