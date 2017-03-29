453d832e6d74eed28fcbcd360bf2bfd7511a8d55
Karl Rohr <kjrohr@fullsail.edu>
2017-03-29 13:26:40 -0400

    Changed sql again

--- swiss.php
+++ swiss.php
@@ -1,7 +1,7 @@
 <?php
   require_once 'dbconfig.php';
 
-  $res=mysql_query("SELECT first_name,last_name FROM t_players");
+  $res=mysql_query("SELECT first_name,last_name FROM t_players WHERE first_name!=''");
   $row=mysql_fetch_array($res);
   $count = mysql_num_rows($res);
 ?>
