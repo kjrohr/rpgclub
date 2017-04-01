7188dbb7fab7b95ba0444fdb0b9e7ef2befbf7c7
Karl Rohr <kjrohr@fullsail.edu>
2017-03-31 22:02:14 -0400

    testing putting checked values into an array

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   var add_button = $("#add_button");
+  var players;
 
   add_button.on("click", function(){
-    console.log("Add button clicked");
+    $('input:checked').each(function() {
+    players.push($(this).attr('value'));
+    console.log(players);
+    });
   });
 });
