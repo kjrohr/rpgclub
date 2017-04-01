371ee3747428729ccf1d807498ace8f6203363f2
Karl Rohr <kjrohr@fullsail.edu>
2017-04-01 00:31:08 -0400

    Randomizing the players

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   var add_button = $("#add_button");
   var start_button = $("#start_event");
+  var pair_players;
   var players = [];
 
   add_button.on("click", function(){
@@ -15,11 +16,37 @@ $(document).ready(function(){
   start_button.on("click", function(){
     $("form").remove();
     $(document.body).append("<h2>Active Players</h2><table><tr><th>Player Number</th><th>Player Name</th></tr></table>");
+    $(document.body).append("<button id='pair_players' type='button' name='pair'>Pair Players</button>")
+    pair_players = $("#pair_players");
     for (var i = 0; i < players.length; i++) {
       console.log(players[i]);
         $("table").append("<tr><td>" + (i+1) + "</td><td>" + players[i] + "</td>");
     }
   });
+
+  pair_players.on("click", function(){
+    players = shuffle(players);
+    console.log(players);
+  });
+
+  function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
 });
